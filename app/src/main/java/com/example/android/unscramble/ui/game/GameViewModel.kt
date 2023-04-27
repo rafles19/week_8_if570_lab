7c23921abccdb807fcf8d41cca810b1a2e655d82
Rafles Kristiyanto <sitiora19@gmail.com>
2023-04-28 02:05:12 +0700

    Initial commit

---
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.android.unscramble.ui.game
+
+import androidx.lifecycle.ViewModel
+import androidx.fragment.app.viewModels
+
+
+class GameViewModel: ViewModel() {
+    private val viewModel: GameViewModel by viewModels()
+
+}
